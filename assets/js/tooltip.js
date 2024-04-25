18a2fb927c1065cf1927f2073d42c2b255e1000a
RicardoEstevesDias <restevesdias@gmail.com>
2024-04-25 17:02:47 +0200

    STYLE: Modification de la page bts

---
+++ assets/js/tooltip.js
@@ -0,0 +1,11 @@
+/*const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl, {
+    'custom-class': 'custom-tooltip',
+}))*/
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl, {
+    'customClass': 'custom-tooltip'
+  })
+})
